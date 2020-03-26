e2b851e32230c68af6439f5c704ecc51f905aad1
mehul5005 <62698869+mehul5005@users.noreply.github.com>
2020-03-26 17:12:56 +0530

    Add files via upload
    1st time commit on git

---
+++ HR.CommonUtility/DBConnectionMySql.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.CommonUtility
+{
+    public class DBConnectionMySql : IDBConnection
+    {
+        //public string Connect()
+        //{
+        //    return "server=172.16.2.129;port=3306;database=hr_proj;user=root;password=root2233;SslMode=None;";
+        //}
+
+        //public int ConnectionTimeout()
+        //{
+        //    return 30;
+        //}
+
+        DbContextOptionsBuilder dbContextOptionsBuilder;
+
+        public string ConnectionString { get; private set; }
+
+        //public string ConnectionString { get; set; }
+
+        public string Connect()
+        {
+            ConnectionString = "server=172.16.2.129;port=3306;database=hr_proj;user=root;password=root2233;SslMode=None;";
+            if (!dbContextOptionsBuilder.IsConfigured)
+            {
+                dbContextOptionsBuilder.UseMySQL(ConnectionString);
+            }
+            return ConnectionString;
+        }
+
+        public int ConnectionTimeout()
+        {
+            return 30;
+        }
+    }
+}
